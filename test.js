912c8cf000705aa57f9c086b0a05b8faa6232879
Jorge Epuñan <juanbrujo@gmail.com>
2016-03-16 08:58:43 -0300

    README updated. tnx to @mtavano

--- test.js
+++ test.js
@@ -1,21 +1,21 @@
-var number = require('./numberToCLPFormater.js');
+var number = require('./numberToCLPFormater.js').numberToCLPFormater;
 
 // Test 1: default
 console.log('\n# Test 1: 876142 -> $876.142');
-console.log( number.numberToCLPFormater(876142) );
+console.log( number(876142) );
 
 // Test 2: change currency symbol
 console.log('\n# Test 2: 981426719 -> CLP$981.426.719');
-console.log( number.numberToCLPFormater(981426719, 'CLP$') );
+console.log( number(981426719, 'CLP$') );
 
 // Test 3: display cents, remove currency symbol
 console.log('\n# Test 3: 1001 -> 1.001,00');
-console.log( number.numberToCLPFormater(1001, false, true) );
+console.log( number(1001, false, true) );
 
 // Test 4: input is not a number
 console.log('\n# Test 4: "lorem ipsum" -> [error]');
-console.log( number.numberToCLPFormater('lorem ipsum') );
+console.log( number('lorem ipsum') );
 
 // Test 5: negative number
 console.log('\n# Test 5: -89163729 -> $-89.163.729');
-console.log( number.numberToCLPFormater(-89163729) );
+console.log( number(-89163729) );
